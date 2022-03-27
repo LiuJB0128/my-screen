086d5bd97eb35e306048a7e4c001f8583e64f14e
LiuJB0128 <1178570567@qq.com>
2022-03-27 15:19:45 +0800

    set page width and height

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m deploy &&
-git remote add origin git@github.com:FrankFang/Se1uqKnmKDZS.git &&
+git remote add origin git@github.com:LiuJB0128/big-screen.git &&
 git push -uf origin master:gh-pages &&
 cd -;
