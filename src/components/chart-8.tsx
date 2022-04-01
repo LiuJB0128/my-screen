cd5569edde0e81c69decfab3e97e210f2f65d6d9
LiuJB0128 <1178570567@qq.com>
2022-04-02 00:30:42 +0800

    使用地图时出现了错误，未解决

--- src/components/chart-8.tsx
+++ src/components/chart-8.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, {useEffect, useRef} from 'react';
+import * as echarts from 'echarts';
+import east from '../geo/data.json';
+import {px} from '../shared/px';
 
 export const Chart8 = () => {
+  const divRef = useRef(null);
+  echarts.registerMap('CN', east);
+  useEffect(() => {
+    const myChart = echarts.init(divRef.current);
+    myChart.setOption({
+      xAxis: {show: false},
+      yAxis: {show: false},
+      series: [
+        {
+          type: 'map',
+          mapType: 'CN', // 自定义扩展图表类型
+          data: [
+            {name: '江苏省', value: 1},
+          ],
+          label: {show: false, color: 'white'},
+          itemStyle: {
+            areaColor: '#010D3D',
+
+            borderColor: '#01A7F7',
+            emphasis: {
+              label: {color: 'white'},
+              areaColor: '#5470C6',
+            },
+          }
+        },
+
+
+      ]
+    });
+  }, []);
   return (
-    <div className="chart3">1</div>
+    <div className="chart3">
+      <div ref={divRef} className="chart"/>
+    </div>
   )
 };
