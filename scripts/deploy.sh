29702e23fbfecfc964b9e2245d50b40753257606
LiuJB0128 <1178570567@qq.com>
2022-04-03 11:44:37 +0800

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m deploy &&
-git remote add origin git@github.com:LiuJB0128/big-screen.git &&
-git push -uf origin master:gh-pages &&
+git remote add origin git@github.com:LiuJB0128/my-screen-website.git &&
+git branch -M main &&
+git push -u origin main &&
 cd -;
